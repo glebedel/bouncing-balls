89ae9d81a01eed2970476b7fad718d33e57dbf12
Guillaume <guillaume.lebedel@gmail.com>
2018-03-23 09:40:34 +0000

    update bookmarklet

--- demo/bookmarklet.js
+++ demo/bookmarklet.js
@@ -1,4 +1,7 @@
 (function() {
+  var fn = function() {
+    var bouncingBalls = new BouncingBalls();
+  };
   var styleId = "bouncing-balls-style";
   if (!document.getElementById(styleId)) {
     var style = document.createElement("style");
@@ -6,5 +9,17 @@
     style.innerHTML =
       ".bouncing-balls-canvas{position:absolute;top:0px;left:0px;";
     document.body.appendChild(style);
+  }
+  var scriptId = "bouncing-balls-lib";
+  if (!document.getElementById(script)) {
+    var script = document.createElement("script");
+    script.id = scriptId;
+    script.type = "text/javascript";
+    script.src =
+      "https://rawgit.com/glebedel/bouncing-balls/master/dist/bouncingballs.js";
+    document.body.appendChild(script);
+    script.onload = fn;
+  } else {
+    fn();
   }
 })();
